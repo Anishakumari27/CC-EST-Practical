7c8d34c69073c450a9d8042d8b68e41cabcf30d3
Anishakumari27 <23BAI70337@cuchd.in>
2025-11-14 12:36:27 +0530

    Create 239.py

---
+++ 239.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        dq = deque()     
+        result = []
+
+        for i in range(len(nums)):
+            while dq and dq[0] < i - k + 1:
+                dq.popleft()
+            while dq and nums[i] >= nums[dq[-1]]:
+                dq.pop()
+
+            dq.append(i)
+
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+
+        return result
